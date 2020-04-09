faebb3cbbc6f22a18947fac0ac9589184b4b5472
Pinefoot <jameslcarpino@gmail.com>
2020-04-09 10:36:26 -0700

    new tests working.

--- src/components/AnimalForm.test.js
+++ src/components/AnimalForm.test.js
@@ -1,3 +1,26 @@
-test('fake test', () =>{
+// import the right stuff!
+import React from 'react';
+import AnimalForm from './AnimalForm';
+import { render } from '@testing-library/react';
 
-});
+test('Current Animals Test Species header is appearing', () => {
+    // arrange - render the component
+    // act - no action to take
+    // assert - get the text from the DOM and expect it to be in the document
+    const { getByText } = render(<AnimalForm />);
+    const headerText = getByText(/current animals test species/i);
+    expect(headerText).toBeVisible();
+  });
+
+  test('inputs are visible', () =>{
+    const { getByLabelText } = render(<AnimalForm/>);
+
+    const labelText = getByAllLabelText(/species/i);
+    expect(labelText).toBeVisible();
+
+  })
+
+
+  test('form submit adds new animals to list', () =>{
+
+  })
